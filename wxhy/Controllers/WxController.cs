b3f31873c17698d3f68e06ada46b442050ef89fb
luoyq <waiterlyq@163.com>
2017-09-24 16:17:07 +0800

    提交

--- wxhy/Controllers/WxController.cs
+++ wxhy/Controllers/WxController.cs
@@ -6,6 +6,8 @@ using System.Web.Mvc;
 using wxhy.Models;
 using Wxlib;
 using Newtonsoft.Json;
+using System.Configuration;
+using System.Net;
 using Loglib;
 
 namespace wxhy.Controllers
@@ -20,14 +22,27 @@ namespace wxhy.Controllers
             {
                 return HttpNotFound();
             }
-
-            string wujson = WxUtil.GetWxUserInfo(WxUtil.GetOpenIdAccess_Token(strcode));
-            MyLog.writeLog(wujson);
-            if (wujson == null)
-            {
-                return HttpNotFound();
-            }
-            return RedirectToAction("Create", "lycustomers", new { wu = wujson });
+            string appid = ConfigurationManager.AppSettings["appid"];
+            string appsecret = ConfigurationManager.AppSettings["appsecret"];
+            MyLog.writeLog(appid);
+            MyLog.writeLog(appsecret);
+            string url = @" https://api.weixin.qq.com/sns/oauth2/access_token?appid=" + appid + "&secret=" + appsecret + "&code=" + strcode + "&grant_type=authorization_code ";
+            MyLog.writeLog(url);
+            WebClient wc = new WebClient();
+            string strReturn = wc.DownloadString(url);
+            MyLog.writeLog(strReturn);
+            WxOpenIdAt woa = JsonConvert.DeserializeObject<WxOpenIdAt>(strReturn);
+            url = @" https://api.weixin.qq.com/sns/userinfo?access_token=" + woa.access_token + "&openid=" + woa.openid + "&lang=zh_CN ";
+            MyLog.writeLog(url);
+            strReturn = wc.DownloadString(url);
+            MyLog.writeLog(strReturn);
+            //string wujson = WxUtil.GetWxUserInfo(WxUtil.GetOpenIdAccess_Token(strcode));
+            //MyLog.writeLog(wujson);
+            //if (wujson == null)
+            //{
+            //    return HttpNotFound();
+            //}
+            return RedirectToAction("Create", "lycustomers");
         }
 
     }
